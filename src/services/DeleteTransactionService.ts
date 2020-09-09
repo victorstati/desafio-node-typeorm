3e73bacf2f967f086da7b388a42dfb49fee897d7
Victor Stati <victorstti@outlook.com>
2020-09-09 14:03:56 -0300

    finished

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import transactionsRouter from '../routes/transactions.routes';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if(!transaction){
+      throw new AppError('Transaction does not exists.');
+    }
+
+    const response = await transactionRepository.remove(transaction);
   }
 }
 
